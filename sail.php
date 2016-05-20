ff51d4537f79d620f5c4ece33972bda9639d3433
Breeze <zhangrcheng@gmail.com>
2016-05-20 16:55:56 +0800

    去除自动写日志的功能

--- sail.php
+++ sail.php
@@ -25,25 +25,7 @@ if(sizeof($usernameOrEmails) === sizeof($passwords)
         }
         // 2. Daily CheckIn
         dailyCheckIn();
-
-        // 3. Diary
-        if(in_array(date('N'), ['6','7'])){
-            echo 'No need for weekend!'.PHP_EOL;
-        } else{
-            // Check if sailed
-            $hasSailed = hasSailed();
-            // if not
-            if($hasSailed){
-                echo 'Diary has been written!'.PHP_EOL;
-            } else{
-                // Get csrfToken
-                $csrfToken = prePost();
-                // Create sail article
-                $newSailResponse = newSail();
-                echo 'Diary created!'.PHP_EOL;
-            }
-        }
-        // 4. logout
+        // 3. logout
         logout();
     }
 } else{
@@ -120,92 +102,6 @@ function dailyCheckIn()
     $curl->newRequest('GET', $checkInUrl)
         ->setHeader('User-Agent', $userAgent)
         ->setCookies($cookies)->send();
-}
-
-
-/**
- * Check if sailed
- * @global \anlutro\cURL\cURL $curl
- * @global string $sessionArr
- * @global strting $userAgent
- * @global string $username
- * @return bool
- */
-function hasSailed()
-{   
-    global $curl, $cookies, $userAgent, $username;
-    $myListUrl = fetchFullUrl("/member/". $username);
-    $myListResponse = $curl->newRequest('GET', $myListUrl)
-            ->setHeader('User-Agent', $userAgent)
-            ->setCookies($cookies)->send();
-    if($myListResponse->statusCode !== 200){
-        throw new Exception('列表读取异常', $myListResponse->statusCode);
-    }
-    $body = $myListResponse->toArray()['body'];
-    $listDivPst = strpos($body, "<div class=\"list\">");
-    if($listDivPst === false){
-        throw new Exception('列表读取异常', $myListResponse->statusCode);
-    }
-    $listStart = strpos($body, '<ul>', $listDivPst) + mb_strlen("<ul>");
-    $listEnd = strpos($body, "</ul>", $listStart);
-    $listPart = substr($body, $listStart, $listEnd - $listStart);
-    $firstSailPst = strpos($listPart, getenv('AUTO_TAG'));
-    if($firstSailPst === false){
-        return false;
-    }
-    $dateStart = strpos($listPart, "</span>", $firstSailPst);
-    $dateEnd = strpos($listPart, "</span>", $dateStart + mb_strlen("</span>"));
-    $dateStrWithTime = trim(substr($listPart, $dateStart + mb_strlen("</span>"),
-            $dateEnd-$dateStart-mb_strlen("</span>")));
-    $dateStr = substr($dateStrWithTime, 0, strpos($dateStrWithTime, " "));
-    $today = date('Y-m-d');
-    return $today == $dateStr;
-}
-
-/**
- * Get csrfToken
- * @global \anlutro\cURL\cURL $curl
- * @global array $cookies
- * @global string $userAgent
- * @return string
- */
-function prePost()
-{
-    global $curl, $cookies, $userAgent;
-    $prePostUrl = fetchFullUrl('post');
-    $prePostResponse = $curl->newRequest('GET', $prePostUrl, [
-        'type' => 4,
-        'tags' => getenv('AUTO_TAG'). ',段落'
-    ])->setCookies($cookies)->setHeader('User-Agent', $userAgent)->send();
-    $body = $prePostResponse->toArray()['body'];
-    $startPst = strpos($body, "onclick=\"AddArticle.add(null,'");
-    $endPst = strpos($body, "'", $startPst + mb_strlen("onclick=\"AddArticle.add(null,'"));
-    $csrfToken = substr($body, $startPst + mb_strlen("onclick=\"AddArticle.add(null,'"), 
-            $endPst - $startPst - mb_strlen("onclick=\"AddArticle.add(null,'"));
-    return $csrfToken;
-}
-
-/**
- * Create a new article
- * @global \anlutro\cURL\cURL $curl
- * @global array $cookies
- * @global string $userAgent
- * @global string $csrfToken
- */
-function newSail(){
-    global $curl, $cookies, $userAgent, $csrfToken;
-    $addArticleUrl = fetchFullUrl('/article');
-    $curl->newJsonRequest('POST', $addArticleUrl, [
-        'articleTitle' => date('Y-m-d'),
-        'articleContent' => '###### ['. getenv('AUTO_CONTENT'). '](https://github.com/breezecoder/auto_sail)',
-        'articleTags' => getenv('AUTO_TAG').",段落",
-        'articleCommentable' => true,
-        'articleType' => 4,
-        'articleRewardContent' => '',
-        'articleRewardPoint' => ''
-    ])->setHeader('Referer', fetchFullUrl('post'))
-      ->setHeader('csrfToken', $csrfToken)
-      ->setCookies($cookies)->setHeader('User-Agent', $userAgent)->send();
 }
 
 /**
